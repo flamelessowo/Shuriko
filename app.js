679f7d68e731c9d42725a8289e0caa37939feb38
flamelessowo <78107022+flamelessowo@users.noreply.github.com>
2022-08-02 23:56:08 +0300

    configuration fix

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+'use strict';
 require('dotenv').config();
 
 const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
 
 const prefix = "~";
+
+
+client.on('ready', () => {
+  console.log('ready steady go');
+})
 
 client.login(process.env.BOT_TOKEN);
