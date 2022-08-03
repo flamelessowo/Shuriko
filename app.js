258fd4b0f306ab5f70e01fe78f40af00460a5aad
flamelessowo <78107022+flamelessowo@users.noreply.github.com>
2022-08-03 21:55:28 +0300

    add message handler

--- app.js
+++ app.js
@@ -11,4 +11,19 @@ client.on('ready', () => {
   console.log('ready steady go');
 })
 
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return;
+  if (message.content.startsWith(prefix)) {
+    const command = message.content.split(" ")[0].replace(prefix, "").toLowerCase();
+    switch(command) {
+      case 'weather':
+        // do something
+        return;
+      case 'another':
+        // do something else
+        return;
+    }
+  }
+})
+
 client.login(process.env.BOT_TOKEN);
